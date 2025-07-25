22c7315fe018d6f2257d4d0cceaa3c67b6840a24
Andres Gaviria <cgaviria930@gmail.com>
2025-07-25 15:00:49 -0500

    login temporal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+
+    const confirmar = () => {
+        console.log("Inicio de sesión confirmado");
+    };
+
     return (
         <form className="flex flex-col items-center justify-center h-screen">
             <div className="my-auto flex flex-col gap-y-8 items-center justify-center bg-white/5 py-10 w-10/12 md:w-1/3 mx-auto rounded-lg">
@@ -16,9 +22,10 @@ export default function Home() {
                     <input id="Contraseña"  autoComplete="off" className="mx-auto outline-none focus:ring-blue-500 focus:border-blue-500 block dark:focus:ring-blue-500 dark:focus:border-blue-500 border-white border-2 rounded-md py-1.5 pl-2 pr-7 tracking-wider w-10/12 md:w-2/3" type="password" placeholder="********" required/>
                 </div>
 
-                <button className="bg-blue-500 tracking-wider text-white font-semibold py-2 px-4 rounded-md w-10/12 md:w-2/3 hover:bg-blue-600 transition-colors duration-300">
+                {/* cambiar a boton */}
+                <Link href="/home" className="bg-blue-500 tracking-wider text-white font-semibold py-2 px-4 rounded-md w-10/12 md:w-2/3 hover:bg-blue-600 transition-colors duration-300">
                     Iniciar Sesión
-                </button>
+                </Link>
             </div>
         </form>
     );

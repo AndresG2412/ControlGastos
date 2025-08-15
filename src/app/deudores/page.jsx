e52494e5d5e2361ee90e90ce3e47207268031a1a
Andres Gaviria <cgaviria930@gmail.com>
2025-08-14 22:22:34 -0500

    pendiente 2

--- src/app/deudores/page.jsx
+++ src/app/deudores/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+// mejorar la tabla y el tachado
+
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { getAuth } from "firebase/auth";

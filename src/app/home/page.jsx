88b236c35c6c603858ffbb049b7678815022ecbd
Andres Gaviria <cgaviria930@gmail.com>
2025-08-14 22:22:39 -0500

    pendiente 3

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+// hacer la tabla mensual una sola tabla
+
 import { useState, useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "../components/Navbar";
